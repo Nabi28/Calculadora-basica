28288f14fa5b76420dd8fbdee1fc88d81e877501
Nabi28 <grijalbadfereshtte03@gmail.com>
2025-05-22 09:01:24 -0600

    Funcion suma terminada + incorporada gitinore

--- calculadara.cpp
+++ calculadara.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-
 using namespace std;
+
+int suma(int a, int b)
+{
+
+    return a + b;
+}
 
 int main()
 {
 
-    cout << "Hola clase!!!.";
+    cout << "Hola clase !!" << endl;
+    cout << "La suma de 1 y 2 es " << suma(1, 2) << "!!";
 
     return 0;
 }
